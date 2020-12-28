54a497a1f7ed6d64c1e37058a7eba632a2697be6
Islam Dudaev <islamwolf23@gmail.com>
2020-12-28 00:12:52 +0000

    Finish router module

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { relative } from 'path';
+
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -10,9 +13,13 @@ import { RecipeService } from '../recipe.service';
 export class RecipeListComponent implements OnInit {
   public recipes: Recipe[];
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService, private router: Router, private route: ActivatedRoute) { }
 
   public ngOnInit(): void {
     this.recipes = this.recipeService.getRecipes();
+  }
+
+  public onNewRecipe(): void {
+    this.router.navigate(['new'], { relativeTo: this.route });
   }
 }
